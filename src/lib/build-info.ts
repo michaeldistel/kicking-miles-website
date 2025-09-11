733f6af1ec535d8df917d1d9f13c3e12a8bb773b
Michael Distel <michael@michaeldistel.com>
2025-09-11 13:01:13 +0800

    Update configuration version and add trailing slash redirect

--- src/lib/build-info.ts
+++ src/lib/build-info.ts
@@ -1,2 +1,2 @@
 // This file is auto-generated at build time
-export const BUILD_COMMIT = 'b45a434';
+export const BUILD_COMMIT = '279e2d5';
--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,4 +1,5 @@
 export const prerender = true;
+export const trailingSlash = 'never';
 
 export async function load({ url }) {
   return {
